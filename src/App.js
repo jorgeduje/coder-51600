656a168843cdbe31c137832288e839e9d445b9c4
jorgeduje <jorgeduje4@gmail.com>
2023-03-11 13:26:49 -0300

    se trabajo con formularios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./Components/Cart/Cart";
+import Form from "./Components/Form/Form";
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import Login from "./Components/Login/Login";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
 
         <Route path="/login" element={<Login />} />
+        <Route path="/formulario" element={<Form />} />
 
         <Route path="*" element={<h1> error 404: Not found </h1>} />
       </Routes>
