b00afee0e32672ee327d0aecbe450861c3a1859d
jorgeduje <jorgeduje4@gmail.com>
2023-03-18 11:29:32 -0300

    se da estilos a cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,30 +1,44 @@
+import { Button } from "@mui/material";
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-
+import "./Cart.css";
 const Cart = () => {
-
   const { cart, clearCart, getTotalPrice } = useContext(CartContext);
 
-  const precioTotal = getTotalPrice()
+  const precioTotal = getTotalPrice();
 
   return (
-    <div
-      style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}
-    >
-      {cart.map((elemento) => {
-        return (
-          <div style={{ border: "2px solid black" }} key={elemento.id}>
-            <h2>{elemento.title}</h2>
-            <img src={elemento.img} alt="" style={{ width: "200px" }} />
-            <h3>Cantidad: {elemento.quantity}</h3>
-            <h3>{elemento.price}</h3>
+    <div className="cart-container">
+      <div className="container-items">
+        {cart.map((item) => {
+          return (
+            <div key={item.id} className="cart-item">
+              <img src={item.img} alt="" />
+              <div className="cart-item-info">
+                <h2>{item.name}</h2>
+                <h2>${item.price}.-</h2>
+                <h2>Unidades: {item.quantity}</h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cart-info">
+        <h3>Precio total: {getTotalPrice()}</h3>
+        <h3>Descuento: - </h3>
+        <h3>Precio final: -</h3>
+
+        {cart.length > 0 && (
+          <div className="btn-cart">
+            <Button variant="contained">Comprar</Button>
+            <Button onClick={() => clearCart()} variant="contained">
+              Vaciar carrito
+            </Button>
           </div>
-        );
-      })}
+        )}
 
-
-<h1>El total del carrito es: {precioTotal}</h1>
-      <button onClick={clearCart}>Limpiar carrito</button>
+        <h1>El total del carrito es ${getTotalPrice()}</h1>
+      </div>
     </div>
   );
 };
