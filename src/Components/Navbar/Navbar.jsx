a9aeee80fe38e9703661f1640073dc27b3009207
jorgeduje <jorgeduje4@gmail.com>
2023-03-25 13:58:53 -0300

    se quita logica de categorias, y se mockea el arr de categorias

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,35 +3,52 @@ import styles from "./Navbar.module.css";
 import CartWidget from "../CartWidget/CartWidget";
 
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "../../firebaseConfig";
 
 const Navbar = ({ children }) => {
-  const [categoryList, setCategoryList] = useState([]);
-  const [cate, setCate] = useState([]);
+  // const [categoryList, setCategoryList] = useState([]);
+  // const [cate, setCate] = useState([]);
 
-  useEffect(() => {
-    const itemsCollection = collection(db, "categories");
-    getDocs(itemsCollection).then((res) => {
-      let arrayCategories = res.docs.map((category) => {
-        return {
-          ...category.data(),
-          id: category.id,
-        };
-      });
-      setCategoryList(arrayCategories);
-      // ESTO ES NUEVO
-      const firstCat = categoryList.length > 0 && categoryList.find(e => e.title === "Todas")
-      const otherCat = categoryList.length > 0 &&  categoryList.filter(e => e.title !== "Todas")
-      if(categoryList.length > 0){
-        setCate([firstCat, ...otherCat])
-      }
-    });
+  // useEffect(() => {
+  //   const itemsCollection = collection(db, "categories");
+  //   getDocs(itemsCollection).then((res) => {
+  //     let arrayCategories = res.docs.map((category) => {
+  //       return {
+  //         ...category.data(),
+  //         id: category.id,
+  //       };
+  //     });
+  //     setCategoryList(arrayCategories);
+  //     // ESTO ES NUEVO
+  //     const firstCat = categoryList.length > 0 && categoryList.find(e => e.title === "Todas")
+  //     const otherCat = categoryList.length > 0 &&  categoryList.filter(e => e.title !== "Todas")
+  //     if(categoryList.length > 0){
+  //       setCate([firstCat, ...otherCat])
+  //     }
+  //   });
 
   
-  }, [categoryList]);
+  // }, [categoryList]);
+  const cate = [ 
+    {
+      title: "Todas",
+      path: "/",
+      id: 1
+    },
+    {
+      title: "Deportivas",
+      path: "/category/deportivas",
+      id: 2
+    },
+    {
+      title: "Urbanas",
+      path: "/category/urbanas",
+      id: 3
+    }
+  ]
 
   return (
     <div>
