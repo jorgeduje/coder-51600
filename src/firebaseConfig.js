f54adfccb4f721eba80a247e33ae44494921c5ec
jorgeduje <jorgeduje4@gmail.com>
2023-03-25 14:36:30 -0300

    se utilizan variables de entorno para proteger info sensible

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVMQFrCkNwYXUIRTrY3lXoGtTHe9Nft2M",
-  authDomain: "react-45090.firebaseapp.com",
-  projectId: "react-45090",
-  storageBucket: "react-45090.appspot.com",
-  messagingSenderId: "990392665216",
-  appId: "1:990392665216:web:0dbc170537be3fc8e79265"
+  apiKey: process.env.REACT_APP_API_KEY ,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID ,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
